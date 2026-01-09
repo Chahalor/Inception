21c4ee45854c1e41c3ac7ec335bc52137b25d9ab
nduvoid <nduvoid@student.42mulhouse.fr>
2026-01-09 11:41:45 +0100

    remove all trace of .env

---
+++ srcs/requirements/worldpress/conf/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+WP_DIR="/var/www/html"
+
+echo "[WordPress] Waiting for MariaDB..."
+
+while ! mysqladmin ping -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent; do
+	sleep 1
+done
+
+echo "[WordPress] MariaDB is available"
+
+if [ ! -f "$WP_DIR/wp-config.php" ]; then
+	echo "[WordPress] Installing WordPress"
+
+	curl -s https://wordpress.org/latest.tar.gz | tar xz --strip 1 -C "$WP_DIR"
+
+	cp /wp-config.php "$WP_DIR/wp-config.php"
+fi
+
+chown -R www-data:www-data "$WP_DIR"
+
+echo "[WordPress] Starting PHP-FPM"
+exec php-fpm7.4 -F
