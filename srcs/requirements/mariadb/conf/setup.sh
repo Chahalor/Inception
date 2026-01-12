9bec7ca70dfdb9d1aa72d17b4c321d54a30707ad
nduvoid <nduvoid@student.42mulhouse.fr>
2026-01-12 12:57:32 +0100

    auto push

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -28,7 +28,7 @@ done
 
 echo "[MariaDB] Configuring database"
 
-mysql --protocol=socket -S -u root <<-SQL
+mysql --protocol=socket -S "$SOCKET" -u root <<-SQL
 	CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 	GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
