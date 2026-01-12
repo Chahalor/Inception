4dddf56d3a449e2827c8eff09f40c572fb633c9c
nduvoid <nduvoid@student.42mulhouse.fr>
2026-01-12 12:56:09 +0100

    auto push

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -28,7 +28,7 @@ done
 
 echo "[MariaDB] Configuring database"
 
-mysql --socket="$SOCKET" -u root <<-SQL
+mysql --protocol=socket -S -u root <<-SQL
 	CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 	GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
@@ -42,3 +42,69 @@ exec mysqld \
 	--user=mysql \
 	--bind-address=0.0.0.0 \
 	--console
+
+
+# #!/bin/sh
+
+# set -e
+
+# DATADIR="/var/lib/mysql"
+# SOCKET="/run/mysqld/mysqld.sock"
+
+# mkdir -p /run/mysqld
+# mkdir -p ${DATADIR}
+# chown -R mysql:mysql /run/mysqld
+# chown -R mysql:mysql ${DATADIR}
+
+# if [ ! -d "${DATADIR}/mysql" ]; then
+# 	echo "[MariaDB] Initialisation du data directory"
+
+# 	mysql_install_db \
+# 		--user=mysql \
+# 		--datadir=${DATADIR}
+
+# 	echo "[MariaDB] Démarrage temporaire (bootstrap)"
+# 	mysqld --user=mysql --skip-networking &
+
+# 	# while ! mysqladmin ping --silent; do
+# 	# 	sleep 1
+# 	# done
+# 	while [ ! -S "$SOCKET" ]; do
+# 		sleep 1
+# 	done
+
+
+# 	echo "[MariaDB] Configuration SQL"
+
+# # 	mysql << EOF # TODO: retry all commented lignes later (addapt them to the new env vars idiot)
+# # CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+# # CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+# # GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+
+# # ALTER USER 'root'@'localhost'
+# # IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';
+
+# # FLUSH PRIVILEGES;
+# # EOF
+# 	mysql --protocol=socket -S "$SOCKET" << EOF
+# CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+
+# CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+# GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+
+# DROP USER IF EXISTS 'root'@'localhost';
+# CREATE USER 'root'@'localhost'
+# IDENTIFIED WITH mysql_native_password
+# BY '${MYSQL_ROOT_PASSWORD}';
+
+# FLUSH PRIVILEGES;
+# EOF
+
+
+# 	echo "[DEBUG] SQL applied"	# rm
+# 	echo "[MariaDB] Arrêt du serveur temporaire"
+# 	mysqladmin shutdown
+# fi
+
+# echo "[MariaDB] Lancement final"
+# exec mysqld --user=mysql --bind-address=0.0.0.0
