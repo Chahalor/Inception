f11e084164842b44296e2b8941f9afecc44d8e8e
nduvoid <nduvoid@student.42mulhouse.fr>
2026-01-13 09:59:53 +0100

    auto push

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -8,8 +8,8 @@ define('DB_HOST', getenv('DB_HOST'));
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
-define('WP_HOME', 'http://nduvoid.42.fr');
-define('WP_SITEURL', 'http://nduvoid.42.fr');
+define('WP_HOME', 'https://nduvoid.42.fr');
+define('WP_SITEURL', 'https://nduvoid.42.fr');
 
 $table_prefix = 'wp_';
 
